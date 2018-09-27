467cc48fdb1d130c6711ae461c1f0da0fc307d48
saidmysr20 <saidmysr20@gmail.com>
2018-09-27 18:18:49 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,6 @@
 # Write your code here!
 require "pry"
-<<<<<<< HEAD
+
 
 def game_hash
   games = {
@@ -138,6 +138,15 @@ def num_points_scored(given_name)
   end
 end
 
+def player_by_number(player_number) 
+  
+  
+  
+end
+
+player_by_number(33)
+
+
 def shoe_size(given_name)
   points = 0 
     game_hash.each do |where, stats|
@@ -254,142 +263,141 @@ end
 
 
 
-=======
->>>>>>> 5d82449ef2e418fdce468a12e82216cd662fc134
 
-def game_hash
-  games = {
-  :home => {
-    :team_name => "Brooklyn Nets",
-    :colors =>["Black","White"] ,
-    :players => {
-      "Alan Anderson" =>{
-        :number => 0,
-        :shoe => 16,
-        :points => 22,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 3,
-        :blocks => 1,
-        :slam_dunks => 1
-      },
-      "Reggie Evans" =>{
-         :number => 30,
-        :shoe => 14,
-        :points => 12,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 12,
-        :blocks => 12,
-        :slam_dunks => 7
-      },
-      "Brook Lopez" => {
-         :number => 11,
-        :shoe => 17,
-        :points => 17,
-        :rebounds => 19,
-        :assists => 10,
-        :steals => 3,
-        :blocks => 1,
-        :slam_dunks => 15
-      },
-      "Mason Plumlee" =>{
-         :number => 1,
-        :shoe => 19,
-        :points => 26,
-        :rebounds => 12,
-        :assists => 6,
-        :steals => 3,
-        :blocks => 8,
-        :slam_dunks => 5
-      },
-      "Jason Terry" =>{
-         :number => 31,
-        :shoe => 15,
-        :points => 19,
-        :rebounds => 2,
-        :assists => 2,
-        :steals => 4,
-        :blocks => 11,
-        :slam_dunks => 1
-      }
-    }
-  },
-  :away => {
-    :team_name => "Charlotte Hornets",
-    :colors =>["Turquoise","Purple"] ,
-    :players => {
-      "Jeff Adrien" =>{
-         :number => 4,
-        :shoe => 18,
-        :points => 10,
-        :rebounds => 1,
-        :assists => 1,
-        :steals => 2,
-        :blocks => 7,
-        :slam_dunks => 2
-      },
-      "Bismak Biyombo" =>{
-         :number => 0,
-        :shoe => 16,
-        :points => 12,
-        :rebounds => 4,
-        :assists => 7,
-        :steals => 7,
-        :blocks => 15,
-        :slam_dunks => 10
-      },
-      "DeSagna Diop" => {
-         :number => 2,
-        :shoe => 14,
-        :points => 24,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 4,
-        :blocks => 5,
-        :slam_dunks => 5
-      },
-      "Ben Gordon" =>{
-         :number => 8,
-        :shoe => 15,
-        :points => 33,
-        :rebounds => 3,
-        :assists => 2,
-        :steals => 1,
-        :blocks => 1,
-        :slam_dunks => 0
-      },
-      "Brendan Haywood" =>{
-         :number => 33,
-        :shoe => 15,
-        :points => 6,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 22,
-        :blocks => 5,
-        :slam_dunks => 12
-      }
-    }
-   }
-  }
-end
 
-def num_points_scored(given_name)
-  points = 0 
-    game_hash.each do |where, stats|
-      stats.each do |info, info_stats|
-        if info == :players 
-          info_stats.each do |points,statistics|
-            statistics.each do |player_stats, value|
-              if player_stats == :points 
-               binding.pry
-            end
-          end
-        end
-      end
-    end
-  end
-end
+# def game_hash
+#   games = {
+#   :home => {
+#     :team_name => "Brooklyn Nets",
+#     :colors =>["Black","White"] ,
+#     :players => {
+#       "Alan Anderson" =>{
+#         :number => 0,
+#         :shoe => 16,
+#         :points => 22,
+#         :rebounds => 12,
+#         :assists => 12,
+#         :steals => 3,
+#         :blocks => 1,
+#         :slam_dunks => 1
+#       },
+#       "Reggie Evans" =>{
+#         :number => 30,
+#         :shoe => 14,
+#         :points => 12,
+#         :rebounds => 12,
+#         :assists => 12,
+#         :steals => 12,
+#         :blocks => 12,
+#         :slam_dunks => 7
+#       },
+#       "Brook Lopez" => {
+#         :number => 11,
+#         :shoe => 17,
+#         :points => 17,
+#         :rebounds => 19,
+#         :assists => 10,
+#         :steals => 3,
+#         :blocks => 1,
+#         :slam_dunks => 15
+#       },
+#       "Mason Plumlee" =>{
+#         :number => 1,
+#         :shoe => 19,
+#         :points => 26,
+#         :rebounds => 12,
+#         :assists => 6,
+#         :steals => 3,
+#         :blocks => 8,
+#         :slam_dunks => 5
+#       },
+#       "Jason Terry" =>{
+#         :number => 31,
+#         :shoe => 15,
+#         :points => 19,
+#         :rebounds => 2,
+#         :assists => 2,
+#         :steals => 4,
+#         :blocks => 11,
+#         :slam_dunks => 1
+#       }
+#     }
+#   },
+#   :away => {
+#     :team_name => "Charlotte Hornets",
+#     :colors =>["Turquoise","Purple"] ,
+#     :players => {
+#       "Jeff Adrien" =>{
+#         :number => 4,
+#         :shoe => 18,
+#         :points => 10,
+#         :rebounds => 1,
+#         :assists => 1,
+#         :steals => 2,
+#         :blocks => 7,
+#         :slam_dunks => 2
+#       },
+#       "Bismak Biyombo" =>{
+#         :number => 0,
+#         :shoe => 16,
+#         :points => 12,
+#         :rebounds => 4,
+#         :assists => 7,
+#         :steals => 7,
+#         :blocks => 15,
+#         :slam_dunks => 10
+#       },
+#       "DeSagna Diop" => {
+#         :number => 2,
+#         :shoe => 14,
+#         :points => 24,
+#         :rebounds => 12,
+#         :assists => 12,
+#         :steals => 4,
+#         :blocks => 5,
+#         :slam_dunks => 5
+#       },
+#       "Ben Gordon" =>{
+#         :number => 8,
+#         :shoe => 15,
+#         :points => 33,
+#         :rebounds => 3,
+#         :assists => 2,
+#         :steals => 1,
+#         :blocks => 1,
+#         :slam_dunks => 0
+#       },
+#       "Brendan Haywood" =>{
+#         :number => 33,
+#         :shoe => 15,
+#         :points => 6,
+#         :rebounds => 12,
+#         :assists => 12,
+#         :steals => 22,
+#         :blocks => 5,
+#         :slam_dunks => 12
+#       }
+#     }
+#   }
+#   }
+# end
+
+# def num_points_scored(given_name)
+#   points = 0 
+#     game_hash.each do |where, stats|
+#       stats.each do |info, info_stats|
+#         if info == :players 
+#           info_stats.each do |points,statistics|
+#             statistics.each do |player_stats, value|
+#               if player_stats == :points 
+#               binding.pry
+#             end
+#           end
+#         end
+#       end
+#     end
+#   end
+# end
 
 
 
